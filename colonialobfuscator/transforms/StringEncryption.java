c58cd531edf521580e3c269366d0c1c3f74a0d79
ColonialBuilders <65742743+ColonialBuilders@users.noreply.github.com>
2021-01-23 19:38:45 -0500

    Add files via upload

--- colonialobfuscator/transforms/StringEncryption.java
+++ colonialobfuscator/transforms/StringEncryption.java
@@ -11,6 +11,8 @@ import javax.crypto.spec.SecretKeySpec;
 
 import colonialobfuscator.utils.NodeUtils;
 import colonialobfuscator.utils.OutputUtil;
+
+import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.Label;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
@@ -50,17 +52,79 @@ public class StringEncryption implements ClassModifier {
 
 	}
 */
-	private static String XOR(int i, int j, String string, int k, int l) {
+	private static String XOR(int i, int j, String string, int k, int l, char[] s) {
 		StringBuilder sb = new StringBuilder();
+		int i1 = 0;
 		for(char c : string.toCharArray()) {
-			sb.append((char)(((c ^ (i ^ k)) ^ j) ^ l));
+			sb.append((char)((((c ^ s[i1 % s.length]) ^ (i ^ k + i1)) ^ j) ^ l));
+			i1++;
 		}
 		return sb.toString();
 	}
+	
+	private static String EncryptKey(String s, char[] b) {
+		 final char[] charArray = s.toCharArray();
+	        final int i = charArray.length;
+	        for (int n = 0; i > n; ++n) {
+	            final int n2 = n;
+	            final char c = charArray[n2];
+	            char c2 = '\0';
+	            switch (n % 7) {
+	                case 0: {
+	                    c2 = b[0];
+	                    break;
+	                }
+	                case 1: {
+	                    c2 = b[1];
+	                    break;
+	                }
+	                case 2: {
+	                    c2 = b[2];
+	                    break;
+	                }
+	                case 3: {
+	                    c2 = b[3];
+	                    break;
+	                }
+	                case 4: {
+	                    c2 = b[4];
+	                    break;
+	                }
+	                case 5: {
+	                    c2 = b[5];
+	                    break;
+	                }
+	                default: {
+	                    c2 = b[6];
+	                    break;
+	                }
+	            }
+	            charArray[n2] = (char)(c ^ c2);
+	        }
+	        return new String(charArray).intern();
+	}
+	
+	
 	@Override
 	public void modify(ClassNode node) {
 	try {
 		if(node.methods != null && node.methods.size() > 0) {
+			
+			String key = NameGen.colonial() + NameGen.String(100);
+			Random ran = new Random();
+			char[] key2 = new char[] {(char) ran.nextInt(126), (char) ran.nextInt(126), (char) ran.nextInt(126)
+					, (char) ran.nextInt(126), (char) ran.nextInt(126), (char) ran.nextInt(126), (char) ran.nextInt(126)};
+			
+	//		String NAME2 = NameGen.colonial() + NameGen.String(2);
+			String NAME3 = NameGen.colonial() + NameGen.String(1);
+		    
+			
+			
+			
+			{
+				FieldVisitor fieldVisitor = node.visitField(ACC_STATIC, NAME3, "[C", null, null);
+				fieldVisitor.visitEnd();
+				}
 			
 			String name = NameGen.String(4);
 			for(MethodNode mn : node.methods) {
@@ -74,7 +138,7 @@ public class StringEncryption implements ClassModifier {
 						InsnList il = new InsnList();
 						il.add(new LdcInsnNode(k1));
 						il.add(new LdcInsnNode(k2));
-						il.add(new LdcInsnNode(XOR(k1, k2, s, k3, k4)));
+						il.add(new LdcInsnNode(XOR(k1, k2, s, k3, k4, key.toCharArray())));
 						il.add(new LdcInsnNode(k3));
 						il.add(new LdcInsnNode(k4));
 					//	methodVisitor.visitMethodInsn(INVOKESTATIC, "test/TEST", "XOR", "(IILjava/lang/String;II)Ljava/lang/String;", false);
@@ -86,7 +150,7 @@ public class StringEncryption implements ClassModifier {
 				});
 			}
 			
-			
+			{
 			MethodVisitor methodVisitor = node.visitMethod(ACC_PRIVATE | ACC_STATIC, name, "(IILjava/lang/String;II)Ljava/lang/String;", null, null);
 			methodVisitor.visitCode();
 			Label label0 = new Label();
@@ -97,29 +161,42 @@ public class StringEncryption implements ClassModifier {
 			methodVisitor.visitVarInsn(ASTORE, 5);
 			Label label1 = new Label();
 			methodVisitor.visitLabel(label1);
+			methodVisitor.visitInsn(ICONST_0);
+			methodVisitor.visitVarInsn(ISTORE, 6);
+			Label label2 = new Label();
+			methodVisitor.visitLabel(label2);
 			methodVisitor.visitVarInsn(ALOAD, 2);
 			methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "toCharArray", "()[C", false);
 			methodVisitor.visitInsn(DUP);
-			methodVisitor.visitVarInsn(ASTORE, 9);
+			methodVisitor.visitVarInsn(ASTORE, 10);
 			methodVisitor.visitInsn(ARRAYLENGTH);
-			methodVisitor.visitVarInsn(ISTORE, 8);
+			methodVisitor.visitVarInsn(ISTORE, 9);
 			methodVisitor.visitInsn(ICONST_0);
-			methodVisitor.visitVarInsn(ISTORE, 7);
-			Label label2 = new Label();
-			methodVisitor.visitJumpInsn(GOTO, label2);
+			methodVisitor.visitVarInsn(ISTORE, 8);
 			Label label3 = new Label();
-			methodVisitor.visitLabel(label3);
-			methodVisitor.visitFrame(Opcodes.F_FULL, 10, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "java/lang/String", Opcodes.INTEGER, Opcodes.INTEGER, "java/lang/StringBuilder", Opcodes.TOP, Opcodes.INTEGER, Opcodes.INTEGER, "[C"}, 0, new Object[] {});
-			methodVisitor.visitVarInsn(ALOAD, 9);
-			methodVisitor.visitVarInsn(ILOAD, 7);
-			methodVisitor.visitInsn(CALOAD);
-			methodVisitor.visitVarInsn(ISTORE, 6);
+			methodVisitor.visitJumpInsn(GOTO, label3);
 			Label label4 = new Label();
 			methodVisitor.visitLabel(label4);
+			methodVisitor.visitFrame(Opcodes.F_FULL, 11, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "java/lang/String", Opcodes.INTEGER, Opcodes.INTEGER, "java/lang/StringBuilder", Opcodes.INTEGER, Opcodes.TOP, Opcodes.INTEGER, Opcodes.INTEGER, "[C"}, 0, new Object[] {});
+			methodVisitor.visitVarInsn(ALOAD, 10);
+			methodVisitor.visitVarInsn(ILOAD, 8);
+			methodVisitor.visitInsn(CALOAD);
+			methodVisitor.visitVarInsn(ISTORE, 7);
+			Label label5 = new Label();
+			methodVisitor.visitLabel(label5);
 			methodVisitor.visitVarInsn(ALOAD, 5);
+			methodVisitor.visitVarInsn(ILOAD, 7);
+			methodVisitor.visitFieldInsn(GETSTATIC, node.name, NAME3, "[C");
 			methodVisitor.visitVarInsn(ILOAD, 6);
+			methodVisitor.visitFieldInsn(GETSTATIC, node.name, NAME3, "[C");
+			methodVisitor.visitInsn(ARRAYLENGTH);
+			methodVisitor.visitInsn(IREM);
+			methodVisitor.visitInsn(CALOAD);
+			methodVisitor.visitInsn(IXOR);
 			methodVisitor.visitVarInsn(ILOAD, 0);
 			methodVisitor.visitVarInsn(ILOAD, 3);
+			methodVisitor.visitVarInsn(ILOAD, 6);
+			methodVisitor.visitInsn(IADD);
 			methodVisitor.visitInsn(IXOR);
 			methodVisitor.visitInsn(IXOR);
 			methodVisitor.visitVarInsn(ILOAD, 1);
@@ -129,24 +206,154 @@ public class StringEncryption implements ClassModifier {
 			methodVisitor.visitInsn(I2C);
 			methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "append", "(C)Ljava/lang/StringBuilder;", false);
 			methodVisitor.visitInsn(POP);
-			Label label5 = new Label();
-			methodVisitor.visitLabel(label5);
-			methodVisitor.visitIincInsn(7, 1);
-			methodVisitor.visitLabel(label2);
-			methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
-			methodVisitor.visitVarInsn(ILOAD, 7);
-			methodVisitor.visitVarInsn(ILOAD, 8);
-			methodVisitor.visitJumpInsn(IF_ICMPLT, label3);
 			Label label6 = new Label();
 			methodVisitor.visitLabel(label6);
-			methodVisitor.visitLineNumber(14, label6);
+			methodVisitor.visitIincInsn(6, 1);
+			Label label7 = new Label();
+			methodVisitor.visitLabel(label7);
+			methodVisitor.visitIincInsn(8, 1);
+			methodVisitor.visitLabel(label3);
+			methodVisitor.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+			methodVisitor.visitVarInsn(ILOAD, 8);
+			methodVisitor.visitVarInsn(ILOAD, 9);
+			methodVisitor.visitJumpInsn(IF_ICMPLT, label4);
+			Label label8 = new Label();
+			methodVisitor.visitLabel(label8);
 			methodVisitor.visitVarInsn(ALOAD, 5);
 			methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/StringBuilder", "toString", "()Ljava/lang/String;", false);
 			methodVisitor.visitInsn(ARETURN);
-			Label label7 = new Label();
-			methodVisitor.visitLabel(label7);
-			methodVisitor.visitMaxs(4, 10);
+			Label label9 = new Label();
+			methodVisitor.visitLabel(label9);
+			methodVisitor.visitMaxs(5, 11);
 			methodVisitor.visitEnd();
+		}
+			{
+				{
+			    	//MethodVisitor methodVisitor = node.visitMethod(ACC_PRIVATE | ACC_STATIC, NAME2, "()V", null, null);
+					MethodNode methodVisitor = new MethodNode();
+					methodVisitor.visitCode();
+					methodVisitor.visitLdcInsn(EncryptKey(key, key2));
+					methodVisitor.visitInsn(ICONST_M1);
+					Label label0 = new Label();
+					methodVisitor.visitJumpInsn(GOTO, label0);
+					Label label1 = new Label();
+					methodVisitor.visitLabel(label1);
+					methodVisitor.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[] {"java/lang/String"});
+					
+					
+					methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "toCharArray", "()[C", false);
+					methodVisitor.visitFieldInsn(PUTSTATIC, node.name, NAME3, "[C");
+					
+				//	methodVisitor.visitVarInsn(ASTORE, 1);
+					
+					Label label2 = new Label();
+					methodVisitor.visitJumpInsn(GOTO, label2);
+					methodVisitor.visitLabel(label0);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 0, new Object[] {}, 2, new Object[] {"java/lang/String", Opcodes.INTEGER});
+					methodVisitor.visitInsn(SWAP);
+					methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "toCharArray", "()[C", false);
+					methodVisitor.visitInsn(DUP);
+					methodVisitor.visitInsn(ARRAYLENGTH);
+					methodVisitor.visitInsn(SWAP);
+					methodVisitor.visitInsn(ICONST_0);
+					methodVisitor.visitVarInsn(ISTORE, 0);
+					Label label3 = new Label();
+					methodVisitor.visitJumpInsn(GOTO, label3);
+					Label label4 = new Label();
+					methodVisitor.visitLabel(label4);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 3, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C"});
+					methodVisitor.visitInsn(DUP);
+					methodVisitor.visitVarInsn(ILOAD, 0);
+					Label label5 = new Label();
+					methodVisitor.visitLabel(label5);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 5, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER});
+					methodVisitor.visitInsn(DUP2);
+					methodVisitor.visitInsn(CALOAD);
+					methodVisitor.visitVarInsn(ILOAD, 0);
+					methodVisitor.visitIntInsn(BIPUSH, 7);
+					methodVisitor.visitInsn(IREM);
+					Label label6 = new Label();
+					Label label7 = new Label();
+					Label label8 = new Label();
+					Label label9 = new Label();
+					Label label10 = new Label();
+					Label label11 = new Label();
+					Label label12 = new Label();
+					methodVisitor.visitTableSwitchInsn(0, 5, label12, new Label[] { label6, label7, label8, label9, label10, label11 });
+					methodVisitor.visitLabel(label6);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 6, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER, Opcodes.INTEGER});
+					methodVisitor.visitIntInsn(BIPUSH, key2[0]);
+					Label label13 = new Label();
+					methodVisitor.visitJumpInsn(GOTO, label13);
+					methodVisitor.visitLabel(label7);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 6, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER, Opcodes.INTEGER});
+					methodVisitor.visitIntInsn(BIPUSH, key2[1]);
+					methodVisitor.visitJumpInsn(GOTO, label13);
+					methodVisitor.visitLabel(label8);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 6, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER, Opcodes.INTEGER});
+					methodVisitor.visitIntInsn(BIPUSH, key2[2]);
+					methodVisitor.visitJumpInsn(GOTO, label13);
+					methodVisitor.visitLabel(label9);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 6, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER, Opcodes.INTEGER});
+					methodVisitor.visitIntInsn(BIPUSH, key2[3]);
+					methodVisitor.visitJumpInsn(GOTO, label13);
+					methodVisitor.visitLabel(label10);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 6, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER, Opcodes.INTEGER});
+					methodVisitor.visitIntInsn(BIPUSH, key2[4]);
+					methodVisitor.visitJumpInsn(GOTO, label13);
+					methodVisitor.visitLabel(label11);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 6, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER, Opcodes.INTEGER});
+					methodVisitor.visitIntInsn(BIPUSH, key2[5]);
+					methodVisitor.visitJumpInsn(GOTO, label13);
+					methodVisitor.visitLabel(label12);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 6, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER, Opcodes.INTEGER});
+					methodVisitor.visitIntInsn(BIPUSH, key2[6]);
+					methodVisitor.visitLabel(label13);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 7, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C", "[C", Opcodes.INTEGER, Opcodes.INTEGER, Opcodes.INTEGER});
+					methodVisitor.visitInsn(IXOR);
+					methodVisitor.visitInsn(I2C);
+					methodVisitor.visitInsn(CASTORE);
+					methodVisitor.visitIincInsn(0, 1);
+					methodVisitor.visitLabel(label3);
+					methodVisitor.visitFrame(Opcodes.F_FULL, 1, new Object[] {Opcodes.INTEGER}, 3, new Object[] {Opcodes.INTEGER, Opcodes.INTEGER, "[C"});
+					methodVisitor.visitInsn(SWAP);
+					methodVisitor.visitInsn(DUP_X1);
+					methodVisitor.visitVarInsn(ILOAD, 0);
+					methodVisitor.visitJumpInsn(IF_ICMPGT, label4);
+					methodVisitor.visitTypeInsn(NEW, "java/lang/String");
+					methodVisitor.visitInsn(DUP_X1);
+					methodVisitor.visitInsn(SWAP);
+					methodVisitor.visitMethodInsn(INVOKESPECIAL, "java/lang/String", "<init>", "([C)V", false);
+					methodVisitor.visitMethodInsn(INVOKEVIRTUAL, "java/lang/String", "intern", "()Ljava/lang/String;", false);
+					methodVisitor.visitInsn(SWAP);
+					methodVisitor.visitInsn(POP);
+					methodVisitor.visitInsn(SWAP);
+					methodVisitor.visitInsn(POP);
+					methodVisitor.visitJumpInsn(GOTO, label1);
+					methodVisitor.visitLabel(label2);
+					methodVisitor.visitFrame(Opcodes.F_CHOP,1, null, 0, null);
+				//	methodVisitor.visitVarInsn(ALOAD, 1);
+				//	methodVisitor.visitInsn(RETURN);
+					
+					 MethodNode clInit = NodeUtils.getMethod(node, "<clinit>");
+		             if (clInit == null) {
+		                 clInit = new MethodNode(Opcodes.ACC_STATIC, "<clinit>", "()V", null, new String[0]);
+		                 node.methods.add(clInit);
+		             }
+		             if (clInit.instructions == null)
+		                 clInit.instructions = new InsnList();
+		             
+		             
+		             if (clInit.instructions == null || clInit.instructions.getFirst() == null) {
+		                 clInit.instructions.add(methodVisitor.instructions);
+		                 clInit.instructions.add(new InsnNode(Opcodes.RETURN));
+		             } else {
+		                 clInit.instructions.insertBefore(clInit.instructions.getFirst(), methodVisitor.instructions);
+		             }
+					
+					
+				}
+			}
 			
 			
 		}
